7a016eb02ce4a20bbdba7fc4153079a47b9ba14d
Amithab <amiquest12@gmail.com>
2020-04-15 04:50:53 +0000

    trying to track nested functions and macros

---
+++ e_simple_macro2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_START(int* begin, int* end) {
+}
+
+extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_STOP() {
+}
+
+void iterate(vector<int> &cs) {
+	for (int i = 0; i < cs.size(); i++) {
+		cs[i]++;
+	}
+	return;
+}
+
+
+void dosom() {
+	int size = 10000;
+	vector<int> c (size, 0);
+	DUMP_ACCESS_START(&c[0], &c[size-1]);
+	for (int i = 0; i < c.size(); i++) {
+		c[i]++;
+	}
+	DUMP_ACCESS_STOP();
+}
+// Nested start stop across functions
+int main(int argc, char *argv[]) {
+	int size = 100;
+	vector<int> c (size, 0);
+	//iterate(&c[0], &c[size-1], c);
+	DUMP_ACCESS_START(&c[0], &c[size-1]);
+	for ( int i = 0; i < c.size(); i++) {
+		c[i]++;
+	}
+	dosom();
+	DUMP_ACCESS_STOP();
+	return 0;
+}
---
+++ e_simple_macro3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_START(int* begin, int* end) {
+}
+
+extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_STOP() {
+}
+
+void iterate(vector<int> &cs) {
+	for (int i = 0; i < cs.size(); i++) {
+		cs[i]++;
+	}
+}
+
+
+void dosom() {
+	int size = 10000;
+	vector<int> c (size, 0);
+	for (int i = 0; i < c.size(); i++) {
+		c[i]++;
+	}
+}
+// Single range across nested function
+int main(int argc, char *argv[]) {
+	int size = 100;
+	vector<int> c (size, 0);
+	//iterate(&c[0], &c[size-1], c);
+	DUMP_ACCESS_START(&c[0], &c[size-1]);
+	for ( int i = 0; i < c.size(); i++) {
+		c[i]++;
+	}
+	iterate(c);
+	DUMP_ACCESS_STOP();
+	return 0;
+}
