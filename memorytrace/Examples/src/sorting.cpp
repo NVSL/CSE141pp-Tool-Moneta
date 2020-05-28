36b845782ba4c9b84e249bfdef6eb3aa11e93b21
Amithab <amiquest12@gmail.com>
2020-05-28 17:48:52 +0000

    added header file, cleanup notebook, included header in sorting

--- memorytrace/Examples/src/sorting.cpp
+++ memorytrace/Examples/src/sorting.cpp
@@ -6,16 +6,11 @@
 */
 
 
+#include "pin_macros.h"
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
-
-extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_START_TAG(const char* tag, int* begin, int* end) {}
-extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_STOP_TAG(const char* tag) {}
-extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_START(int* begin, int* end) {}
-extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_STOP() {}
-extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS(int* begin, int* end, int stop_start){}
 
 #define SIZE 1000
 
