fc6c44afe6094f45ea33474793fea8c152783547
Sam <szl005@ucsd.edu>
2020-07-03 20:36:09 +0000

    Fixed formatting and error handling

--- memorytrace/Examples/src/tiling_test.cpp
+++ memorytrace/Examples/src/tiling_test.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,13 +14,16 @@ int main(int argc, char *argv[]){
 	
     int TILE_SIZE = SIZE;
 
-    if(argc > 1){
-        TILE_SIZE = stoi(argv[1]);
+    try{
+        if(argc > 1){
+            TILE_SIZE = max(1, stoi(argv[1]));
+        }
+    } catch(...) {
+        cerr << "Invalid Tile Size\n";
+        exit(-1);
     }
 
-    if(TILE_SIZE < 1){
-        TILE_SIZE = 1;
-    }
+    cout << "Tile Size: " << TILE_SIZE << endl;
 
     vector<vector<int>> scale(SIZE, vector<int> (SIZE));
     vector<int> values(SIZE2);
@@ -40,17 +44,18 @@ int main(int argc, char *argv[]){
 
 
     for(int xx = 0; xx < SIZE; xx += TILE_SIZE){
-    for(int i = 0; i < SIZE2; i++){
-        for(int x = xx; x < SIZE && x < xx + TILE_SIZE; x++){
-            for(int y = 0; y < SIZE; y++){
-                values[i] += scale[x][y];
+        for(int i = 0; i < SIZE2; i++){
+            for(int x = xx; x < SIZE && x < xx + TILE_SIZE; x++){
+                for(int y = 0; y < SIZE; y++){
+                    values[i] += scale[x][y];
+                }
             }
         }
-    }
     }
 
     DUMP_ACCESS_STOP_TAG("scale");
 
     cout << values[0] << endl;
 
+    exit(0);
 }
