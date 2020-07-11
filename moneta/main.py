e00cf9e9fa25985f50756b2d6e1e6211496fdd5b
Amithab <amiquest12@gmail.com>
2020-07-11 04:52:21 +0000

    limit fix

--- moneta/main.py
+++ moneta/main.py
@@ -711,9 +711,11 @@ def generate_plot(trace_name):
   #   plt.gca().set_axis_off()
   #   plt.show()
   
+  x_lim = [df.index.min()[()], df.index.max()[()]]
+  y_lim = [df.Address.min()[()], df.Address.max()[()]]
   #replace checks2 with simple_legend to display the simple_legend over current legend
   plot = df.plot_widget(df.index, df.Address, what='max(Access)',
-                 colormap = custom_cmap, selection=[True],
+                 colormap = custom_cmap, selection=[True], limits = [x_lim, y_lim],
                  backend='bqplot_v2', tool_select=True, legend=checks2, update_stats = updateStats, type='custom_plot1')
   if tag_path:
     for i in range(numTags):
