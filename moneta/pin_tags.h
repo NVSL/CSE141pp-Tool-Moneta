9cd3003b8c3208d9861011d3dd06f674a284322f
jadbarrere <jbarrere@ucsd.edu>
2020-08-12 18:11:31 +0000

    renamed pin_tags.h and vaextended

--- moneta/pin_tags.h
+++ moneta/pin_tags.h
@@ -1,5 +1,5 @@
-#ifndef MEMORYTRACE_PINTOOL_MACROS
-#define MEMORYTRACE_PINTOOL_MACROS
+#ifndef MEMORYTRACE_PINTOOL_TAGS
+#define MEMORYTRACE_PINTOOL_TAGS
 
 extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_START_TAG(const char* tag, const void* begin, const void* end) {}
 extern "C" __attribute__ ((optimize("O0"))) void DUMP_ACCESS_STOP_TAG(const char* tag) {}
