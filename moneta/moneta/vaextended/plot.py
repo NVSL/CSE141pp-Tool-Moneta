7b0d40a2ea917fecc8c8de530c32389e773a1e58
Sam <szl005@ucsd.edu>
2020-09-15 04:45:15 +0000

    Fixed if statement ordering

--- moneta/moneta/vaextended/plot.py
+++ moneta/moneta/vaextended/plot.py
@@ -81,10 +81,11 @@ class PlotBase(widgets.Widget):
                 self.cache_size = kwargs.get('cache_size')
             if 'default_title' in kwargs:
                 self.default_title = kwargs.get('default_title')
-            if 'update_stats' in kwargs:
-                self.update_stats = kwargs.get('update_stats')
             else:
                 self.default_title = 'Moneta'
+            if 'update_stats' in kwargs:
+                self.update_stats = kwargs.get('update_stats')
+
             self.widget = PlotTemplate(components={
                         'main-widget': widgets.VBox([widgets.HBox([self.backend.widget], layout=widgets.Layout(margin="50px 10px 10px 10px")), self.progress, self.output]),
                         'output-widget': self.output,
