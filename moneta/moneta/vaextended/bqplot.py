281ff6f48a7b6a75e6a97b6f37fc90631cd29c1e
Amithab <amiquest12@gmail.com>
2020-09-16 12:48:02 -0700

    100% - legend width fix

--- moneta/moneta/vaextended/bqplot.py
+++ moneta/moneta/vaextended/bqplot.py
@@ -78,7 +78,7 @@ class BqplotBackend(BackendBase):
         self.scales = {'x': self.scale_x, 'y': self.scale_y}
 
         self.figure = plt.figure(self.figure_key, fig=self.figure, scales=self.scales)
-        self.figure.layout.width = '70%'
+        self.figure.layout.width = 'calc(100% - 300px)'
         self.figure.layout.min_height = '800px'
         plt.figure(fig=self.figure)
         #self.figure.padding_y = 0
