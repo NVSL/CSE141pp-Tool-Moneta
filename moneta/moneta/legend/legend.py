cb41b3b2628455bcfabf1a20c61ea69739a9b3f8
Elvis Tran <elt007@ucsd.edu>
2020-09-16 01:33:29 -0700

    Fixed bug where click zoom would display unchecked regions of data

--- moneta/moneta/legend/legend.py
+++ moneta/moneta/legend/legend.py
@@ -24,7 +24,7 @@ class Legend():
         self.widgets = VBox([self.panels, panel_style], layout=Layout(padding='0px', border='1px solid black', width='400px'))
         self.accesses = Accesses(model, self.update_selection)
         self.tags = Tags(model, self.update_selection)
-        self.click_zoom = Click_Zoom(model)
+        self.click_zoom = Click_Zoom(model, self.accesses, self.tags)
         self.add_panel(LEGEND_MEM_ACCESS_TITLE, self.accesses.widgets)
         self.add_panel(LEGEND_TAGS_TITLE, self.tags.widgets)
         self.add_panel(LEGEND_CLICK_ZOOM, self.click_zoom.widgets)
@@ -53,28 +53,11 @@ class Legend():
         for checkbox in self.tags.checkboxes:
             if checkbox.widget.v_model == False:
                 selections.add('(Tag != %s)' % (checkbox.tag_id))
-        return '&'.join(selections)
-
-    def get_select_array(self):
-        self.df = self.model.curr_trace.df
-        selected_bytes = self.df.evaluate(self.df.Bytes, selection=True)
-        selected_access_number = self.df.evaluate(self.df.Access_Number, selection=True)
-        selected_access = self.df.evaluate(self.df.Access, selection=True)
-        selected_array = vaex.from_arrays(Bytes=selected_bytes, Access_Number=selected_access_number, Access=selected_access)
-        #self.df.select('&'.join(selections), mode='replace') # replace not necessary for correctness, but maybe perf?
-        return selected_array
+        return '&'.join(selections) 
 
     @debounced(0.5)
     def update_selection(self): 
-        self.model.curr_trace.df.select(self.get_select_string(), mode='replace') # replace not necessary for correctness, but maybe perf?
-        #selection = self.df[self.model.curr_trace.df[self.get_select_string()]]
-        #self.click_zoom.click_zoom_obj.update_df_selections(self.get_select_array())
-        #print(self.model.curr_trace.df['Access', 'Tag', 'Bytes', 'Access_Number'])
-        self.df = self.model.curr_trace.df
-        selected_bytes = self.df.evaluate(self.df.Bytes, selection=True)
-        #tmp_df = self.df[['Access', 'Tag', 'Bytes', 'Access_Number'], selection=True]
-        #self.df = self.df.extract()
-        #print(tmp_df)
+        self.model.curr_trace.df.select(self.get_select_string(), mode='replace') # replace not necessary for correctness, but maybe perf? 
 
     def add_panel(self, name, contents):
         acc = v.ExpansionPanel(children=[
