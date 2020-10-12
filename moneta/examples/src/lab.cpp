9091eb2639feb91de7a211b9038c17092971f581
Amithab <amiquest12@gmail.com>
2020-10-12 15:52:57 -0700

    removed tags

--- moneta/examples/src/lab.cpp
+++ moneta/examples/src/lab.cpp
@@ -7,9 +7,6 @@ int main() {
   int *A = (int *)malloc(N * N * sizeof(int));
   int *B = (int *)malloc(N * N * sizeof(int));
 
-
-  DUMP_START_SINGLE("A", A, A + N*N - 1);
-  DUMP_START_SINGLE("B", B, B + N*N - 1);
   int sum = 0;
   for (int i = 0; i < 10; i++) {
     for (int row = 0; row < N; row++) {
@@ -18,8 +15,6 @@ int main() {
       }
     }
   }
-  DUMP_STOP("A");
-  DUMP_STOP("B");
 
   std::cout << "Final sum: " << sum << "\n";
   free(A);
