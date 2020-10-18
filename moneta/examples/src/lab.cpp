078145530cc6e421715d534772cbe084057a7d51
Amithab <amiquest12@gmail.com>
2020-10-18 19:44:47 +0000

    stack lab

--- moneta/examples/src/lab.cpp
+++ moneta/examples/src/lab.cpp
@@ -4,18 +4,16 @@
 constexpr int N = 128;
 
 int main() {
-  int *A = (int *)malloc(N * N * sizeof(int));
-  int *B = (int *)malloc(N * N * sizeof(int));
+  int A[N][N];
+  int B[N][N];
 
   int sum = 0;
   for (int row = 0; row < N; row++) {
     for (int col = 0; col < N; col++) {
-      sum += (*(A + N*row + col)) + (*(B + N*col + row));
+      sum += A[row][col] + B[col][row];
     }
   }
 
   std::cout << "Final sum: " << sum << "\n";
-  free(A);
-  free(B);
   return 0;
 }
