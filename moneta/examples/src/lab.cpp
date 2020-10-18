974801c66f5a51ea60d450d880c0f6ebea330064
Amithab <amiquest12@gmail.com>
2020-10-18 18:58:01 +0000

    removed tag

--- moneta/examples/src/lab.cpp
+++ moneta/examples/src/lab.cpp
@@ -9,11 +9,9 @@ int main() {
 
   int sum = 0;
   for (int row = 0; row < N; row++) {
-    DUMP_START_MULTI("iteration", A, B + N*N-1);
     for (int col = 0; col < N; col++) {
       sum += (*(A + N*row + col)) + (*(B + N*col + row));
     }
-    DUMP_STOP("iteration");
   }
 
   std::cout << "Final sum: " << sum << "\n";
