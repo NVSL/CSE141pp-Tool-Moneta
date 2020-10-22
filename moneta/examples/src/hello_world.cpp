582844d463f7daaf7b18896b6e3d5683ffa82816
Amithab <amiquest12@gmail.com>
2020-10-22 13:59:31 -0700

    fixed unified tags commit

--- moneta/examples/src/hello_world.cpp
+++ moneta/examples/src/hello_world.cpp
@@ -4,13 +4,13 @@
 
 int main() {
   int arr [5] = {0}; // An array of 5 ints
-  DUMP_START_SINGLE("acc_array", arr, arr+4); // arr == first element's address, arr + 4 == fifth element's address
+  DUMP_START("acc_array", arr, arr+4, false); // arr == first element's address, arr + 4 == fifth element's address
   std::cout << "Hello world! " << arr[0] << "\n";
   DUMP_STOP("acc_array");
 
-  DUMP_START_MULTI("loop_array", arr, arr+4); // new tag to indicate different part of the program, multi so shows up as loop_array0
+  DUMP_START("loop_array", arr, arr+4, false); // new tag to indicate different part of the program, multi so shows up as loop_array0
   for (int i = 0; i < 10; i++) {
-    DUMP_START("loop_array"); // using shorthand, same address range of [arr, arr+4]
+    DUMP("loop_array", true); // using shorthand, same address range of [arr, arr+4]
     for (int j = 0; j < 5; j++) { // being a multi tag, each inner loop will fall into `loop_array1`, `loop_array2`, ..., `loop_array10`
       arr[j]+=i;
     }
--- moneta/pin_tags.h
+++ moneta/pin_tags.h
@@ -2,8 +2,8 @@
 #define MONETA_PINTOOL_TAGS
 
 extern "C" {
-  __attribute__ ((optimize("O0"))) void DUMP_START(const char* tag, const void* begin, const void* end, bool add_to_prev) {}
-  __attribute__ ((optimize("O0"))) void DUMP(const char* tag, bool add_to_prev) {}
+  __attribute__ ((optimize("O0"))) void DUMP_START(const char* tag, const void* begin, const void* end, bool create_new) {}
+  __attribute__ ((optimize("O0"))) void DUMP(const char* tag, bool create_new) {}
   __attribute__ ((optimize("O0"))) void DUMP_STOP(const char* tag) {}
 
   __attribute__ ((optimize("O0"))) void FLUSH_CACHE() {}
