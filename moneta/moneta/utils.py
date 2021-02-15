4f7bc99eb6b3be6ba2b9f576768cda9c9ce3c6cb
Sam <szl005@ucsd.edu>
2021-02-15 07:20:51 +0000

    Using return code instead of cerr

--- moneta/moneta/utils.py
+++ moneta/moneta/utils.py
@@ -242,7 +242,7 @@ def run_pintool(w_vals):
   
     os.chdir(MONETA_TOOL_DIR)
   
-    if sub_stderr:
+    if sub_output.returncode:
         print(f"{TextStyle.RED}An error occurred while running your program. "
               f"This is normally caused by Tag typos and/or stopping invalid Tags. "
               f"Double check your program and Pin tags to make sure they are correct.\n\n"
