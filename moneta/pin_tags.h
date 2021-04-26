55911b0334752294a3a697c150c3c52ed515d7f7
Steven Swanson <swanson@cs.ucsd.edu>
2021-04-26 22:54:34 +0000

    cleanup

--- moneta/pin_tags.h
+++ moneta/pin_tags.h
@@ -3,17 +3,16 @@
 #include<cstdio>
 
 extern "C" {
-#define VOLATILE_NOP volatile asm{nop}
 	
-	__attribute__ ((optimize("O0"))) void DUMP_START(const char* tag, const void* begin, const void* end, bool create_new) {VOLATILE_NOP;}
-	__attribute__ ((optimize("O0"))) void M_NEW_TRACE(const char* name) {VOLATILE_NOP;}
+	__attribute__ ((optimize("O0"))) void DUMP_START(const char* tag, const void* begin, const void* end, bool create_new) {}
+	__attribute__ ((optimize("O0"))) void M_NEW_TRACE(const char* name) {}
 	__attribute__ ((optimize("O0"))) void DUMP_START_ALL(const char* tag, bool create_new) {DUMP_START(tag, 0, (void*)-1, create_new);}
-	__attribute__ ((optimize("O0"))) void DUMP_STOP(const char* tag) {VOLATILE_NOP;}
+	__attribute__ ((optimize("O0"))) void DUMP_STOP(const char* tag) {}
 	
-	__attribute__ ((optimize("O0"))) void M_START_TRACE(bool one, bool two) {VOLATILE_NOP;}
-	__attribute__ ((optimize("O0"))) void M_STOP_TRACE(bool one, bool two) {VOLATILE_NOP;}
+	__attribute__ ((optimize("O0"))) void M_START_TRACE(bool one, bool two) {}
+	__attribute__ ((optimize("O0"))) void M_STOP_TRACE(bool one, bool two) {}
 	
-	__attribute__ ((optimize("O0"))) void FLUSH_CACHE() {VOLATILE_NOP;}
+	__attribute__ ((optimize("O0"))) void FLUSH_CACHE() {}
 	void START_TRACE() {
 		M_START_TRACE(true, true);
 	}
