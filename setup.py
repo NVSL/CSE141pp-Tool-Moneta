b747957c7940ca51493c9d058e680b5f585b8a98
Steven Swanson <swanson@cs.ucsd.edu>
2021-04-28 22:37:09 +0000

    Fixed install problems, I think

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages, Extension
 setup(name='Moneta',
       version='1.0',
       packages=find_packages(".", exclude=("vaextended",)),
-      package_dir={'': 'moneta/moneta'},
+      package_dir={'': 'moneta'},
       entry_points={
           'console_scripts' :[
               'mtrace=mtrace:main'
