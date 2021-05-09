c653f7b4067bf4f0e24681390403ba4a62aec0a5
Sam <szl005@ucsd.edu>
2021-05-09 23:46:43 +0000

    Decoupled file browser from moneta

--- moneta/moneta/main.py
+++ moneta/moneta/main.py
@@ -1,15 +1,22 @@
 
 import argparse
+import os
 from importlib import reload
-import moneta.model as model
-import moneta.view as view
+from moneta import Moneta
+from moneta.ipyfilechooser import FileChooser
+from moneta.settings import FC_FILTER
+from moneta.utils import handle_load_trace
 
 def main():
     parser = argparse.ArgumentParser(description='Process main.py flags')
     args = parser.parse_args()
 
-    view.View(model.Model())
-    pass
+    m = Moneta()
+
+    file_chooser = FileChooser(path=os.getcwd(), use_dir_icons=True, filter_pattern=FC_FILTER)
+    file_chooser.register_callback(lambda self_fc: handle_load_trace(self_fc, m))
+    display(file_chooser)
+
 
 if __name__ == '__main__':
     main()
--- moneta/moneta/utils.py
+++ moneta/moneta/utils.py
@@ -1,5 +1,31 @@
+from IPython.display import clear_output, display
+
 def percent_string(count, total):
     return 'N/A' if total == 0 else f'{count/total:06.2%}'
 
 def stats_string(label, count, total):
     return f'{label}: {count} ({percent_string(count, total)})'
+
+def handle_load_trace(file_chooser, model):
+    model.ready_next_trace()
+    clear_output(wait=True)
+    display(file_chooser)
+
+    path = file_chooser.selected_path
+    trace_name = file_chooser.selected_filename
+    print(f'Loading {trace_name}\n')
+
+    err_message = model.load_trace(path, trace_name)
+
+    if err_message is not None:
+        print(err_message)
+        return
+
+    model.create_plot()
+
+    if model.plot is None:
+        print("Couldn't load plot")
+        return
+
+    model.plot.show()
+    model.legend.stats.update(init=True)
