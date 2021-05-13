1cb8443cdd2ddb076d6a547a185f3a76b982db3f
Steven Swanson <swanson@cs.ucsd.edu>
2021-05-13 18:08:23 +0000

    simple python functions to display a trace

--- moneta/moneta/main.py
+++ moneta/moneta/main.py
@@ -4,11 +4,17 @@ from importlib import reload
 import moneta.model as model
 import moneta.view as view
 
+def show_trace(trace_file):
+    view.View(model.Model()).load_file(trace_file)
+
+def select_trace():
+    view.View(model.Model(), file_chooser=True)
+    
 def main():
     parser = argparse.ArgumentParser(description='Process main.py flags')
     args = parser.parse_args()
 
-    view.View(model.Model())
+    view.View(model.Model(), file_chooser=True)
 
 
 if __name__ == '__main__':
