c731a8e0d22f19b069b009c91d2b2a67f4f62fa1
Isaac Bi <31492046+Isaac-Bi@users.noreply.github.com>
2021-08-27 13:59:41 -0700

    Revert "Adding JS Trace "

--- moneta/moneta_tags.js
+++
@@ -1,7 +0,0 @@
-const moneta = require( './build/release/moneta.node');
-
-arr = Array.from({length: 1000}, () => Math.floor(Math.random() * 1000));
-moneta.JS_START_TRACE();
-// moneta.JS_DUMP_START_ALL("sort", true);
-s = arr.sort();
-moneta.JS_DUMP_STOP("sort");
