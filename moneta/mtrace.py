ff39571e99200cd1f48a7d5b75065932b710930a
Steven Swanson <swanson@cs.ucsd.edu>
2022-08-11 21:02:03 -0700

    first step toward removing hdf5 from pintool

--- moneta/mtrace.py
+++ moneta/mtrace.py
@@ -1,8 +1,88 @@
+import logging as log
+import subprocess
+import platform
+import os
+import click
+
+@click.command()
+@click.option("--verbose", "-v", is_flag=True, default=False, help="Be verbose")
+@click.option("--trace", type=str, default="trace", help="trace name")
+@click.option("--cache-line-size", type=int, default=64, help="Cache line size")
+@click.option("--cache-line-count", type=int, default=512, help="cache line count")
+@click.option("--main", type=str, default="main", help="Function to trace")
+@click.option("--memops", default=10000000, help="How many memory operations to trace")
+@click.option("--file-count", default=1, type=int, help="How many trace files to collect")
+@click.option("--skip", default=0, type=int, help="How many memops to skip")
+@click.option("--debug", is_flag=True, default=False, help="Pause so you can attach a debugger")
+@click.option("--tagged-only", is_flag=True, default=False, help="Only record tagged accesses.")
+@click.option("--flush-cache-on-new-file", is_flag=True, default=False, help="Flush cache when you open a new file")
+@click.argument("cmd", nargs=-1)
+def mtrace(*argc, **kwargs) :
+    do_mtrace(*argc, **kwargs)
+    
+def do_mtrace(*argc, **args):
+
+    verbose = args.pop("verbose", False)
+    trace =  args.pop("trace", "trace")
+    cache_line_count = args.pop("cache_line_count", 512)
+    cache_line_size = args.pop("cache_line_size", 64)
+    main = args.pop("main", "main")
+    memops = args.pop("memops", 10000000)
+    file_count = args.pop("file_count", 1)
+    skip = args.pop("skip", 0)
+    debug = args.pop("debug", False)
+    flush_cache_on_new_file = args.pop("flush_cache_on_new_file", False)
+    cmd = args.pop("cmd")
+    jupyter = args.pop("jupyter", True)
+    tagged_only = args.pop("tagged_only", False)
+    if not verbose:
+        log.basicConfig(format="%(levelname)-8s %(message)s", level=log.WARN)
+    else:
+        log.basicConfig(format="{} %(levelname)-8s [%(filename)s:%(lineno)d]  %(message)s".format(platform.node()) if verbose else "%(levelname)-8s %(message)s",
+                        level=log.DEBUG if verbose else log.WARN)
+
+    if os.environ.get("OMP_NUM_THREADS"):
+        log.warn("-------------")
+        log.warn("Overriding OMP_NUM_THREADS by setting it to 1")
+        log.warn("-------------")
+        os.environ["OMP_NUM_THREADS"] = "1"
+
+    pin_cmd =f"{os.environ['PIN_ROOT']}pin.sh -ifeellucky -injection child "
+    tool_cmd=f"-t {os.environ['PIN_ROOT']}source/tools/ManualExamples/obj-intel64/trace_tool.so -name {trace} -file_count {file_count} -cache_lines {cache_line_count} -block {cache_line_size} -start {main} -ol {memops}  -skip {skip}" + (" -tagged-only" if tagged_only else "")
+    if debug:
+        pin_cmd += " -pause_tool 30"
+    if flush_cache_on_new_file:
+        tool_cmd += " -flush-cache-on-new-file"
+
+    app_cmd = ' '.join(cmd)
+
+    run_cmd = f"{pin_cmd} {tool_cmd} -- {app_cmd}"
+
+    files = [f"{trace}.meta",
+             f"{trace}.tags",
+             f"{trace}.hdf5"]
+
+    for f in files:
+        try:
+            if not jupyter:
+                log.info(f"Removing {f}")
+            os.unlink(f)
+        except:
+            pass
+
+    print(f"Running: {run_cmd}")
+    try:
+        if not jupyter:
+            log.info(f"Cache size: {cache_line_count} lines * {int(cache_line_size)} bytes/Line = {int(cache_line_count) * int(cache_line_size)} KB")
+            p = subprocess.run(run_cmd.split())
+        else:
+            p = subprocess.run(run_cmd.split(), check=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+            print(p.stdout.decode())
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode())
+        
 
 import logging as log
-import argparse
-import textwrap
-import sys
 import subprocess
 import platform
 import os
--- setup/trace_tool.cpp
+++ setup/trace_tool.cpp
@@ -13,9 +13,8 @@
 #include <algorithm>
 #include <ctype.h>
 #include <sys/stat.h> // mkdir
-///#include <pthread.h>
+#include "moneta_trace.hpp"
 #include "pin.H"   // Pin
-#include "H5Cpp.h" // Hdf5
 #include<cassert>
 
 VOID Instruction(INS ins, VOID *v);
@@ -54,7 +53,7 @@ const std::string DefaultStartFunction  {"__libc_start_main"};
 constexpr ADDRINT LIMIT {0};
 
 // Output file formatting
-const std::string TraceSuffix    {".hdf5"};
+const std::string TraceSuffix    {".mtrace"};
 const std::string TagFileSuffix  {".tags"}; //csv
 const std::string StatsFileSuffix  {".stats"}; //csv
 const std::string MetaFileSuffix {".meta"}; //txt
@@ -95,9 +94,9 @@ enum {
 	HIT, CAP_MISS, COMP_MISS
 };
 
-static UINT64 curr_traced_lines {0}; // Increment for every write to hdf5 for memory accesses.  Reset when opening a new file.
-static UINT64 total_traced_lines {0}; // Increment for every write to hdf5 for memory accesses
-static UINT64 all_lines {0}; // Increment for every write to hdf5 for memory accesses
+static UINT64 curr_traced_lines {0}; // Increment for every write trace for memory accesses.  Reset when opening a new file.
+static UINT64 total_traced_lines {0}; // Increment for every write to trace for memory accesses
+static UINT64 all_lines {0}; // Increment for every write to trace for memory accesses
 
 // Increment id for every new tag
 static int curr_id {0};
@@ -232,207 +231,31 @@ const std::string LayerColumn{"Layer"};
 /*
  * Use this to handle hdf with write_data_mem and write_data_cache
  */
-class HandleHdf5 {
-	static const unsigned long long int chunk_size = 200000; // Private vars
-	unsigned long long addrs[chunk_size]; // Chunk local vars
-	uint8_t accs[chunk_size];
-	uint8_t threadids[chunk_size];
-	ulong indexes[chunk_size];
-	uint8_t layer[chunk_size];
+class TraceWriter {
 
-	//unsigned long long cache_addrs[chunk_size]; // Chunk local vars - cache
-	// Current access
-	size_t mem_ind = 0;
-	//size_t cache_ind = 0;
-	size_t index = 0; // increment index of each access
-	size_t layer_default = 1;
-	// Memory accesses
-	H5::H5File mem_file;
-	//  H5::DataSet tag_d;
-	H5::DataSet index_d;
-	H5::DataSet acc_d;
-	H5::DataSet addr_d;
-	H5::DataSet threadid_d;
-	H5::DataSet layer_d;
-	// State vars
-	hsize_t curr_chunk_dims [1] = {chunk_size};
-	hsize_t total_ds_dims [1] = {chunk_size};
-	hsize_t offset [1] = {0};
-
-	// Cache accesses
-	/*H5::H5File cache_file;
-	  H5::DataSet cache_d;
-	  // State vars
-	  hsize_t curr_chunk_dims_c [1] = {chunk_size};
-	  hsize_t total_ds_dims_c [1] = {chunk_size};
-	  hsize_t offset_c [1] = {0};*/
-
-	// Open files, dataspace, and datasets
-	void setup_files() {
-		if (HDF_DEBUG) {
-			std::cerr << "Setting up hdf5 files\n";
-		}
-		// Try catch block here highly recommended!!!
-		//H5::Exception::dontPrint(); // Don't print exceptions - Let catch block do it
-		hsize_t idims_t[1] = {1};   // Initial dims
-		H5::DSetCreatPropList plist; // Set chunk size with property list
-		plist.setChunk(1, curr_chunk_dims);
-		hsize_t max_dims[1] = {H5S_UNLIMITED}; // For extendable dataset
-		H5::DataSpace m_dataspace {1, idims_t, max_dims}; // Initial dataspace
-		// Create and write datasets to file
-		index_d = mem_file.createDataSet(IndexColumn.c_str(), H5::PredType::NATIVE_ULLONG, m_dataspace, plist);
-		acc_d =      mem_file.createDataSet(AccessColumn.c_str(),   H5::PredType::NATIVE_UINT8, m_dataspace, plist);
-		threadid_d = mem_file.createDataSet(ThreadIDColumn.c_str(), H5::PredType::NATIVE_UINT8, m_dataspace, plist);
-		addr_d =     mem_file.createDataSet(AddressColumn.c_str(),  H5::PredType::NATIVE_ULLONG, m_dataspace, plist);
-		layer_d = mem_file.createDataSet(LayerColumn.c_str(), H5::PredType::NATIVE_UINT8, m_dataspace, plist);
-
-		//H5::DataSpace c_dataspace {1, idims_t, max_dims};
-		//cache_d = cache_file.createDataSet(CacheAccessColumn.c_str(), H5::PredType::NATIVE_ULLONG, c_dataspace, plist);
-	}
-
-	// Extends dataset and writes stored chunk
-	void extend_write_mem() {
-		if (HDF_DEBUG) {
-			std::cerr << "Extending and writing dataset for memory accesses\n";
-		}
-		acc_d.extend( total_ds_dims ); // Extend size of dataset
-		addr_d.extend( total_ds_dims );
-		threadid_d.extend( total_ds_dims );
-		index_d.extend(total_ds_dims);
-		layer_d.extend(total_ds_dims);
-
-		H5::DataSpace old_dataspace = acc_d.getSpace(); // Get old dataspace
-		H5::DataSpace new_dataspace = {1, curr_chunk_dims}; // Get new dataspace
-		old_dataspace.selectHyperslab( H5S_SELECT_SET, curr_chunk_dims, offset); // Select slab in extended dataset
-		acc_d.write( accs, H5::PredType::NATIVE_UINT8, new_dataspace, old_dataspace); // Write to extended part
-
-		old_dataspace = addr_d.getSpace(); // Rinse and repeat for address
-		old_dataspace.selectHyperslab( H5S_SELECT_SET, curr_chunk_dims, offset);
-		addr_d.write( addrs, H5::PredType::NATIVE_ULLONG, new_dataspace, old_dataspace);
-
-		old_dataspace = threadid_d.getSpace(); // Rinse and repeat for threads
-		old_dataspace.selectHyperslab(H5S_SELECT_SET, curr_chunk_dims, offset);
-		threadid_d.write(threadids, H5::PredType::NATIVE_UINT8, new_dataspace, old_dataspace);
-
-		old_dataspace = index_d.getSpace(); // Rinse and repeat for index
-		old_dataspace.selectHyperslab(H5S_SELECT_SET, curr_chunk_dims, offset);
-		index_d.write(indexes, H5::PredType::NATIVE_ULLONG, new_dataspace, old_dataspace);
-
-		old_dataspace = layer_d.getSpace(); // Rinse and repeat for layer
-		old_dataspace.selectHyperslab(H5S_SELECT_SET, curr_chunk_dims, offset);
-		layer_d.write(layer, H5::PredType::NATIVE_UINT8, new_dataspace, old_dataspace);
-	}
-
-	// Extends dataset and writes stored chunk
-	/*void extend_write_cache() {
-	  if (HDF_DEBUG) {
-	  std::cerr << "Extending and writing dataset for cache\n";
-	  }
-	  cache_d.extend( total_ds_dims_c ); // Extend
-	  H5::DataSpace old_dataspace = cache_d.getSpace(); // Get old dataspace
-	  H5::DataSpace new_dataspace = {1, curr_chunk_dims_c}; // Get new dataspace
-	  old_dataspace.selectHyperslab( H5S_SELECT_SET, curr_chunk_dims_c, offset_c); // Select slab in extended dataset
-	  cache_d.write( cache_addrs, H5::PredType::NATIVE_ULLONG, new_dataspace, old_dataspace); // Write to the extended part
-	  }*/
-
+	int mem_file;
 public:
-	// Default constructor
-	HandleHdf5() : mem_file({"trace.hdf5", H5F_ACC_TRUNC}) { setup_files(); }
-
-	/*// Default constructor - Cache
-	  HandleHdf5() : mem_file({"trace.hdf5", H5F_ACC_TRUNC}),
-	  cache_file({"cache.hdf5", H5F_ACC_TRUNC}) { setup_files(); }*/
-
-	// With names
-	HandleHdf5(std::string h) : mem_file({h.c_str(), H5F_ACC_TRUNC}) { setup_files(); }
-
-	// With names - Cache
-	/*HandleHdf5(std::string h, std::string c) : 
-	  mem_file({h.c_str(), H5F_ACC_TRUNC}),
-	  cache_file({c.c_str(), H5F_ACC_TRUNC}) { setup_files(); }*/
-	// Destructor
-	~HandleHdf5() {
-		if (HDF_DEBUG) {
-			std::cerr << "Hdf5 Destructor\n";
-		}
-		if (mem_ind != 0) { // New data to write
-			curr_chunk_dims[0] = mem_ind; // Not full chunk size
-			total_ds_dims[0]  -= chunk_size - mem_ind;
-
-			extend_write_mem();
-		}
-
-		/*if (cache_ind != 0) { // Same for cache
-		  curr_chunk_dims_c[0] = cache_ind;
-		  total_ds_dims_c[0] -= chunk_size - cache_ind;
-
-		  extend_write_cache();
-		  }*/
-
-		// Close files
-		mem_file.close();
-		//cache_file.close();
-
+	TraceWriter(std::string filename) {
+		mem_file = open(filename, "w");
+		assert(mem_file != -1);
 	}
 
-	// Write data for memory access to file
+	~TraceWriter() {
+		close(memfile);
+	}
+
 	int write_data_mem(ADDRINT address, int access, THREADID threadid) {
-		if (HDF_DEBUG) {
-			std::cerr << "Write to Hdf5 - memory\n";
-		}
-		addrs[mem_ind] = address; // Write to memory first
-		threadids[mem_ind] = (unsigned char)(threadid & 0xff);
-		accs[mem_ind++] = access;
-		index++;
-		indexes[mem_ind] = index;
-		layer[mem_ind] = layer_default;
 
-		if (mem_ind < chunk_size) { // Unless we have reached chunk size
-			return 0;
-		}
-		mem_ind = 0;
-
-		extend_write_mem(); // Then, write to hdf5 file
-      
-		total_ds_dims[0] += chunk_size; // Update size and offset
-		offset[0] += chunk_size;
-		/*try { // Should add this error checking somewhere
-		  } catch( H5::FileIException error ) {
-		  error.printError(); // Add custom messages here?
-		  return -1; // Exit program?
-		  } catch( H5::DataSetIException error ) {// catch failure caused by the DataSet operations
-		  error.printError();
-		  return -1;
-		  } catch( H5::DataSpaceIException error ) {// catch failure caused by the DataSpace operations
-		  error.printError();
-		  return -1;
-		  } catch( H5::DataTypeIException error ) {// catch failure caused by the DataType operations
-		  error.printError();
-		  return -1;
-		  }*/
+		struct trace_entry entry;
+		entry.address = address;
+		entry.acc = access;
+		entry.threadid = threadid;
+		write(mem_file, &entry, sizeof(trace_entry));
 		return 0;
 	}
-
-	// Write data for cache access to file
-	/*int write_data_cache(ADDRINT address) {
-	  if (HDF_DEBUG) {
-	  std::cerr << "Write to Hdf5 - cache\n";
-	  }
-	  cache_addrs[cache_ind++] = address;
-	  if (cache_ind < chunk_size) { 
-	  return 0;
-	  }
-	  cache_ind = 0;
-
-	  extend_write_cache();
-	  total_ds_dims_c[0] += chunk_size;
-	  offset_c[0] += chunk_size;
-	  return 0;
-	  }*/
-
 };
 
-HandleHdf5 *hdf_handler; // One for this pintool
+TraceWriter * trace_writer; // One for this pintool
 
 struct cache_hash { // Could improve on this hash function
 	inline std::size_t operator()(const std::pair<ADDRINT, std::list<ADDRINT>::iterator> & k) const {
@@ -594,13 +417,13 @@ void open_trace_files() {
 	file_count++;
 	  
 	output_tagfile_path = DefaultOutputPath + "/" + s.str() + TagFileSuffix;
-	std::string output_hdf5_trace_path = DefaultOutputPath + "/" + s.str() + TraceSuffix;
+	std::string output_trace_path = DefaultOutputPath + "/" + s.str() + TraceSuffix;
 
 	mkdir(DefaultOutputPath.c_str(), 0755);
 	
 	std::cerr << "Opening trace '" << s.str() << "'.\n";
-	unlink(output_hdf5_trace_path.c_str());
-	hdf_handler = new HandleHdf5(output_hdf5_trace_path);
+	unlink(output_trace_path.c_str());
+	trace_writer = new TraceWriter(output_trace_path);
 
 	for (auto thread: thread_ids) {
 		std::stringstream name;
